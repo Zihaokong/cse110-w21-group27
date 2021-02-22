c1277a592fb9505a8b8046e79843f863516be0ef
Zihaokong <kongzihao99@gmail.com>
2021-02-21 21:37:15 -0800

    Styling enforcement
    Getting ESlinter check and fix style

--- source/main.js
+++ source/main.js
@@ -3,94 +3,43 @@
  * and other Modals for the main page.
  */
 
-
-/// ////// SECTION for Modals////////
 // Get the modal
-
-let modal = document.getElementById("add-task-modal");
-let playModal = document.getElementById("play-modal");
-let taskContent = document.getElementById("task-name");
+const modal = document.getElementById('add-task-modal');
+const playModal = document.getElementById('play-modal');
+// eslint-disable-next-line no-unused-vars
+const taskContent = document.getElementById('task-name');
 
 // Get the button that opens the modal
-let btns = document.getElementsByClassName("add-task-btn");
-let cancelBtns = document.getElementsByClassName("cancel-btn");
-
+const btns = document.getElementsByClassName('add-task-btn');
+const cancelBtns = document.getElementsByClassName('cancel-btn');
 
 // Get the <span> element that closes the modal
-let spanClose = document.getElementsByClassName("close");
+const spanClose = document.getElementsByClassName('close');
 
-//add event listeners
+// add event listeners
 for (let i = 0; i < spanClose.length; ++i) {
-  spanClose[i].addEventListener("click", closeModal);
-  cancelBtns[i].addEventListener("click", closeModal);
-
+  spanClose[i].addEventListener('click', closeModal);
+  cancelBtns[i].addEventListener('click', closeModal);
 }
-
 // Listener for add task modal
 for (let i = 0; i < btns.length; ++i) {
-  btns[i].addEventListener("click", displayAddModal);
+  btns[i].addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
 }
-
-
-/**
- * For scroll to the top, used in Top button
- */
-function scrollFunc() {
-  window.scrollTo(0, 0);
-}
-
-
-/**
- * When the user clicks the button, open the modal 
- */
-function displayAddModal() {
-  modal.style.display = "block";
-}
-
-/**
- * Section for Play Modal
- */
-function displayPlayModal() {
-  playModal.style.display = 'block';
-}
-
-
-/**
- * For showing the taks name, content on the modal
- * @param {*} element The target element that the user wants to start with
- */
-function showModalTask(element) {
-  // get the closest li from where we click and get the p tag in its children
-  const targetName = element.closest("li").getElementsByTagName('p');
-  // make the task name appear in the timer modal
-  document.getElementById('timer-name').innerText = targetName[0].innerHTML;
-  // Retrieving the note in Storage by getting its id
-  const targetID = element.closest("li").getAttribute("id");
-  // get the element Index in the object list
-  const taskStorageIndex = allTasks.findIndex(elem => elem.id === targetID);
-  // make the note from storage appear in the timer modal
-  document.getElementById('timer-note').innerText = allTasks[taskStorageIndex].note;
-}
-
-
-
 
 // When the user clicks on <span> (x), close the modal
 function closeModal() {
-  modal.style.display = "none";
-  playModal.style.display = "none";
+  modal.style.display = 'none';
+  playModal.style.display = 'none';
 }
 
-
-
 /**
- * Close the mocal
- * @param {event} event: Javascript event
+ * Close the modal
+ * @param {event} event: Javascript events
  */
-window.onclick = function (event) {
-  if (event.target == modal) {
+window.onclick = function closeModal2(event) {
+  if (event.target === modal) {
     modal.style.display = 'none';
   }
 };
-
-
--- source/task.js
+++ source/task.js
@@ -3,7 +3,6 @@
  */
 
 // HTML List of all tasks on HTML page
-// eslint-disable-next-line no-undef
 const list = document.querySelector('.task-container');
 list.addEventListener('click', handleEdit);
 
@@ -21,86 +20,81 @@ let allTasks;
  * When loading page, retrive previously stored task from
  * local storage, and render it, delete welcome message
  */
-window.onload = function () {
-
-    var retrievedObject = localStorage.getItem("allTasks");
-    if (!retrievedObject || retrievedObject === "undefined") {
-        allTasks = [];
-    } else {
-        allTasks = JSON.parse(retrievedObject);
-        if (allTasks.length != 0) {
-            welcome.remove();
-        }
-
-        for (let i = 0; i < allTasks.length; i++) {
-            renderTask(allTasks[i]);
-        }
+window.onload = function loadTask() {
+  const retrievedObject = localStorage.getItem('allTasks');
+  if (!retrievedObject || retrievedObject === 'undefined') {
+    allTasks = [];
+  } else {
+    allTasks = JSON.parse(retrievedObject);
+    if (allTasks.length !== 0) {
+      welcome.remove();
     }
+
+    for (let i = 0; i < allTasks.length; i++) {
+      renderTask(allTasks[i]);
+    }
+  }
 };
 
 /**
  * Closing page will save current task and update local storage
  */
-window.onbeforeunload = function () {
-    localStorage.setItem('allTasks', JSON.stringify(allTasks));
+window.onbeforeunload = function storeTask() {
+  localStorage.setItem('allTasks', JSON.stringify(allTasks));
 };
-
 
 /**
  * Add a task to the page and to the global list.
- * @param event
+ * @param {event} event: Javascript events
  */
 function addTask(event) {
-    event.preventDefault();
-    //acquire data from HTML form
-    const taskInput = document.getElementById("task-name");
-    const taskInputNum = document.getElementById("task-num");
-    const taskInputNote = document.getElementById("task-note");
-    const elementID = Math.random().toString(16).slice(2);
-    //create struct and append to global list
-    const newTask = {
-        id: elementID,
-        completed: false,
-        name: taskInput.value,
-        number: taskInputNum.value,
-        current: 0,
-        note: taskInputNote.value
-    }
-    allTasks.push(newTask);
+  event.preventDefault();
+  // acquire data from HTML form
+  const taskInput = document.getElementById('task-name');
+  const taskInputNum = document.getElementById('task-num');
+  const taskInputNote = document.getElementById('task-note');
+  const elementID = Math.random().toString(16).slice(2);
+  // create struct and append to global list
+  const newTask = {
+    id: elementID,
+    completed: false,
+    name: taskInput.value,
+    number: taskInputNum.value,
+    current: 0,
+    note: taskInputNote.value,
+  };
+  allTasks.push(newTask);
 
-    //render HTML on page.
-    renderTask(newTask);
+  // render HTML on page.
+  renderTask(newTask);
 
-    //everything else.
-    taskForm.reset();
-    welcome.remove();
-    closeModal();
+  // everything else.
+  taskForm.reset();
+  welcome.remove();
+  document.getElementById('add-task-modal').style.display = 'none';
 }
 
 /**
  * render a task struct on page, display name and current progress
- * @param {*} newTask: the task struct to render
+ * @param {newTask} newTask: the task struct to render
  */
 function renderTask(newTask) {
-    //  To-do: add real time progress bar percentage display
-    const position = "beforeend";
-    const dragButton = `<span class="p-2 material-icons drag-btn">drag_indicator</span>`;
-    const checkmark =
-        `<span class="p-2 form-check form-check-inline">
+  //  To-do: add real time progress bar percentage display
+  const position = 'beforeend';
+  const dragButton = `<span class="p-2 material-icons drag-btn">drag_indicator</span>`;
+  const checkmark = `<span class="p-2 form-check form-check-inline">
             <input class="form-check-input input-mysize large" type="checkbox" job="check">
             <label for="checkbox"></label>
         </span>`;
-    const todoTask = '<p class="p-2 flex-md-fill text-nowrap task-item">' + newTask.name + '</p>';
-    const progressbar = `
+  const todoTask = `<p class="p-2 flex-md-fill text-nowrap task-item">${newTask.name}</p>`;
+  const progressbar = `
         <div class=" flex-column progress">
-            <div class="p-2 flex-column progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="` + newTask.number + `">25%</div>
+            <div class="p-2 flex-column progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="${newTask.number}">25%</div>
         </div>`;
-    const playButton =
-        `<button class="p-2 bd-highlight btn  play-btn flex-right" type="button">
+  const playButton = `<button class="p-2 bd-highlight btn  play-btn flex-right" type="button">
             <span class="material-icons play-btn" job ="play">play_circle</span>
         </button>`;
-    const editButton =
-        `<div class="p-2 bd-highlight btn-group dropright flex-right">
+  const editButton = `<div class="p-2 bd-highlight btn-group dropright flex-right">
             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="material-icons edit-btn">more_horiz</span>
             </button>
@@ -110,91 +104,106 @@ function renderTask(newTask) {
                 <a class="dropdown-item" href="#" job="delete">Delete</a>
             </div>
         </div>`;
-    list.insertAdjacentHTML(position, '<li id=' + newTask.id + ' class="taskNode d-flex flex-row bd-highlight" draggable = true>' +
-        dragButton + checkmark + todoTask + progressbar + playButton + editButton);
-    renderCheckmark(newTask);
+  list.insertAdjacentHTML(
+    position,
+    `<li id=${newTask.id} class="taskNode d-flex flex-row bd-highlight" draggable = true>${dragButton}${checkmark}${todoTask}${progressbar}${playButton}${editButton}`
+  );
+  renderCheckmark(newTask);
 }
 
 /**
  * Update the checkmark status on the localStorage
- * @param {*} newTask the task node just built from renderTask()
+ * @param {newTask} newTask the task node just built from renderTask()
  */
 function renderCheckmark(newTask) {
-    //setting checkmark
-    const newNode = document.getElementById(newTask.id);
-    //li: dragIcon -> span-checkmark; span: text -> actual checkbox
-    const checkbox = newNode.childNodes[1].childNodes[1];
-    checkbox.checked = newTask.completed;
+  // setting checkmark
+  const newNode = document.getElementById(newTask.id);
+  // li: dragIcon -> span-checkmark; span: text -> actual checkbox
+  const checkbox = newNode.childNodes[1].childNodes[1];
+  checkbox.checked = newTask.completed;
 }
-
-
 
 /**
  * Update the checkmark status on the array for localStorage
- * @param {*} event 
+ * @param {event.target} element: Javascript event.target
  */
 function handleCheck(element) {
-    // Retrieving the note in Storage by getting its id
-    const targetID = element.closest("li").getAttribute("id");
-    // get the element Index in the object list
-    const taskIndex = allTasks.findIndex(elem => elem.id === targetID);
-    allTasks[taskIndex].completed = !allTasks[taskIndex].completed;
+  // Retrieving the note in Storage by getting its id
+  const targetID = element.closest('li').getAttribute('id');
+  // get the element Index in the object list
+  const taskIndex = allTasks.findIndex((elem) => elem.id === targetID);
+  allTasks[taskIndex].completed = !allTasks[taskIndex].completed;
 }
-
 
 /**
  * Click more button, giving user edit and delete options
- * @param {*} event
+ * @param {event} event: Javascript events.
  */
 function handleEdit(event) {
-    //getting which is being clicked
-    let element = event.target;
-    let eleJob;
-    // console.log(element);
-    //job may be undefined
-    if (event.target.attributes.job) {
-        eleJob = event.target.attributes.job.value;
-    }
+  // getting which is being clicked
+  const element = event.target;
+  let eleJob;
+  // console.log(element);
+  // job may be undefined
+  if (event.target.attributes.job) {
+    eleJob = event.target.attributes.job.value;
+  }
 
-    // console.log(eleJob);
-    if (eleJob == "delete") {
-        deleteTask(element);
-    } else if (eleJob == "edit") {
-        displayAddModal();
-    } else if (eleJob == "play") {
-        displayPlayModal();
-        showModalTask(element);
-    } else if (eleJob == "check") {
-        handleCheck(element);
-    }
+  // console.log(eleJob);
+  if (eleJob === 'delete') {
+    deleteTask(element);
+  } else if (eleJob === 'edit') {
+    document.getElementById('add-task-modal').style.display = 'block';
+  } else if (eleJob === 'play') {
+    document.getElementById('play-modal').style.display = 'block';
+    showModalTask(element);
+  } else if (eleJob === 'check') {
+    handleCheck(element);
+  }
 }
 
 /**
  * Delete task
- * @param {*} element
+ * @param {event.target} element: Javascript event.target
  */
 function deleteTask(element) {
-    element.closest("ul").removeChild(element.closest("li"));
-    let name = element.closest("li").children[2].innerHTML;
-    for (let i = 0; i < allTasks.length; i++) {
-        if (allTasks[i].name == name) {
-            allTasks.splice(i, 1);
-            break;
-        }
+  element.closest('ul').removeChild(element.closest('li'));
+  const name = element.closest('li').children[2].innerHTML;
+  for (let i = 0; i < allTasks.length; i++) {
+    // eslint-disable-next-line eqeqeq
+    if (allTasks[i].name == name) {
+      allTasks.splice(i, 1);
+      break;
     }
+  }
 }
 
-///////// SECTION for Drag and Drop ////////
+/**
+ * For showing the task name, content on the modal when going to timer page.
+ * @param {event.target} element: The target element that the user wants to start with
+ */
+function showModalTask(element) {
+  // get the closest li from where we click and get the p tag in its children
+  const targetName = element.closest('li').getElementsByTagName('p');
+  // make the task name appear in the timer modal
+  document.getElementById('timer-name').innerText = targetName[0].innerHTML;
+  // Retrieving the note in Storage by getting its id
+  const targetID = element.closest('li').getAttribute('id');
+  // get the element Index in the object list
+  const taskStorageIndex = allTasks.findIndex((elem) => elem.id === targetID);
+  // make the note from storage appear in the timer modal
+  document.getElementById('timer-note').innerText =
+    allTasks[taskStorageIndex].note;
+}
 
 // getter for the list
-var dropzone = document.getElementById("main-container");
-//getter for the list items
-var nodes = document.getElementsByClassName("taskNode");
+const dropzone = document.getElementById('main-container');
+// getter for the list items
+const nodes = document.getElementsByClassName('taskNode');
 // variable for the selected node to be dragged or moved
-var selectedNode;
+let selectedNode;
 // variable for the position of selected node
-var selectedNodePos = 0;
-
+let selectedNodePos = 0;
 
 // Listener for the dragstart event
 dropzone.addEventListener(
@@ -206,49 +215,49 @@ dropzone.addEventListener(
 );
 
 // Listener for the dragover event
-dropzone.addEventListener("dragover", (event) => {
-    event.preventDefault();
-    whereAmI(event.clientY);
+dropzone.addEventListener('dragover', (event) => {
+  event.preventDefault();
+  whereAmI(event.clientY);
 });
 
 // Listener for the drop event
-dropzone.addEventListener("drop", (event) => {
-    event.preventDefault();
-    dropzone.insertBefore(selectedNode, dropzone.children[selectedNodePos]);
+dropzone.addEventListener('drop', (event) => {
+  event.preventDefault();
+  dropzone.insertBefore(selectedNode, dropzone.children[selectedNodePos]);
 });
 
 /**
  * For measuring the selected node position from the list.
  */
 function establishNodePositions() {
-    for (var i = 0; i < nodes.length; i++) {
-        var element = document.getElementById(nodes[i]["id"]);
-        var position = element.getBoundingClientRect(); //info of the element position on the frame
-        var yTop = position.top;
-        var yBottom = position.bottom;
-        //yCenter
-        nodes[i]["yPos"] = yTop + (yBottom - yTop) / 2;
-    }
+  for (let i = 0; i < nodes.length; i++) {
+    const element = document.getElementById(nodes[i].id);
+    const position = element.getBoundingClientRect(); // info of the element position on the frame
+    const yTop = position.top;
+    const yBottom = position.bottom;
+    // yCenter
+    nodes[i].yPos = yTop + (yBottom - yTop) / 2;
+  }
 }
 
 /**
- * For deciding which position the selected element goes to as measuring which 
+ * For deciding which position the selected element goes to as measuring which
  * is the closest parent and closet children
- * this function will call establishNodePositions() for selected node position. 
- * @param {*} currentYPos 
+ * this function will call establishNodePositions() for selected node position.
+ * @param {event.clickY} currentYPos
  */
 function whereAmI(currentYPos) {
-    establishNodePositions();
-    for (var i = 0; i < nodes.length; i++) {
-        if (nodes[i]["yPos"] < currentYPos) {
-            var nodeAbove = document.getElementById(nodes[i]["id"]);
-            selectedNodePos = i + 1;
-        }
+  establishNodePositions();
+  let nodeAbove;
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i].yPos < currentYPos) {
+      nodeAbove = document.getElementById(nodes[i].id);
+      selectedNodePos = i + 1;
     }
-    // for the top of the list
-    if (typeof nodeAbove === "undefined") {
-        selectedNodePos = 0;
-    }
+  }
+  // for the top of the list
+  if (typeof nodeAbove === 'undefined') {
+    selectedNodePos = 0;
   }
   // for the top of the list
   if (typeof nodeAbove === 'undefined') {
