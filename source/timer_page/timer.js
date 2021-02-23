206f23badaf535aaf2b63f9c5e39c3a5d636598f
anvitaa22 <asekhsar@ucsd.edu>
2021-02-23 10:52:48 -0800

    fail button functionality added (with modal)

--- source/timer_page/timer.js
+++ source/timer_page/timer.js
@@ -1,29 +1,49 @@
-var minutes = 25;
-var seconds = "00";
-let secs = 0;
-let totalsecs = 1500;
 let distractCounter = 0;
+let sessionCounter = 0;
+let isPomo = 0;
+localStorage.setItem('sessionCounter', 0);
+localStorage.setItem('isPomo', 0);
 
 function template(){
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("minutes").innerHTML = "01";
+    document.getElementById("seconds").innerHTML = "00";
     document.getElementById("distraction-btn").innerHTML = "Distraction : " + distractCounter;
-    document.getElementById("title_timer").innerHTML = "25:00" + "- Time To Work!"
+    document.getElementById("title_timer").innerHTML = "01:00" + "- Time To Work!";
+    //setTimeout(function(){ start(0, 59); }, 1000);
+    start(0,59);
 }
 
-function start(){
-    minutes = 24;
-    seconds = 59;
+function start(minutes, seconds){
+    let secs = 0;
+    let totalsecs = (minutes+1) * 60;
+    //var progress_ring_interval = setInterval(updateProgressRing, 10);
+    if(minutes<10){
+        document.getElementById("minutes").innerHTML = "0"+minutes;
+    }
+    else{
+        document.getElementById("minutes").innerHTML = minutes;
+    }
+    if(seconds < 10){
+        document.getElementById("seconds").innerHTML = "0"+seconds;
+        document.getElementById("title_timer").innerHTML = minutes + ":0" + seconds + "- Time To Work!";
+    }
+    else{
+        document.getElementById("seconds").innerHTML = seconds;
+        document.getElementById("title_timer").innerHTML = minutes + ":" + seconds + "- Time To Work!";
+    }
 
+    var minutes_interval = setInterval(minutesTimer, 60000);
+    var seconds_interval = setInterval(secondsTimer, 1000);
     secs = secs + 1;
     perc = 100-(((totalsecs - secs)/totalsecs)*100);
     setProgress(perc);
 
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-    var minutes_interval = setInterval(minutesTimer, 60000);
-    var seconds_interval = setInterval(secondsTimer, 1000);
+    /*function updateProgressRing(){
+        secs = secs + 1;
+        perc = 100-(((totalsecs - secs)/totalsecs)*100);
+        setProgress(perc);
+        console.log("123");
+    }*/
 
     function minutesTimer(){
         minutes = minutes -1;
@@ -36,17 +56,31 @@ function start(){
     }
 
     function secondsTimer(){
+        seconds = seconds -1;
         secs = secs + 1;
         perc = 100-(((totalsecs - secs)/totalsecs)*100);
         setProgress(perc);
-        seconds = seconds -1;
         if(seconds < 10){
             document.getElementById("seconds").innerHTML = "0"+seconds;
             document.getElementById("title_timer").innerHTML = minutes + ":0" + seconds + "- Time To Work!";
             if(seconds == 0){
-                if(minutes == 0){
+                if(minutes <= 0){
                     clearInterval(minutes_interval);
                     clearInterval(seconds_interval);
+                    let counter = localStorage.getItem('sessionCounter');
+                    counter = counter+1;
+                    let pomo = localStorage.getItem('isPomo');
+                    if(pomo == 1){
+                        resetPomo();
+                    }
+                    else{
+                        if(counter <= 3){
+                            startShortBreak();
+                        }
+                        else if(counter == 4){
+                            startLongBreak();
+                        }
+                    }
                 }
                 seconds = 60;
             }
@@ -64,3 +98,16 @@ function distractionCount(){
     document.getElementById("distraction-btn").innerHTML = "Distraction : " + distractCounter;
 }
 
+function displayFailModal(){
+    document.getElementById("failModal").style.display = 'block';
+}
+function failSession(){
+    window.location.href="../index.html";
+}
+function quitFailModal(){
+    document.getElementById("failModal").style.display = 'none';
+}
+
+function resetPomo(){
+    
+}
