d472e86581ce78c1820eeea93b45d32ae44408af
cmwall0605 <cmwall@ucsd.edu>
2021-02-26 12:07:35 -1000

    Updated tests to be more intensive, but are broken

--- tests/task-item.test.js
+++ tests/task-item.test.js
@@ -1,6 +1,15 @@
 const TaskItem = require('../source/task-item');
 
 describe('Pass in to constructor test', () => {
+  beforeEach(() => {
+    document.body.innerHTML =
+      '<div id = "test"> <button id="button" />' +
+      ' <input type="text" id="task-name">' +
+      ' <input type="text" id="task-num">' +
+      ' <input type="text" id="task-note">' +
+      '</div>';
+  });
+
   test('New instance has the correct id and classes', () => {
     const inputTask = {
       id: '05023c2908555',
@@ -14,5 +23,19 @@ describe('Pass in to constructor test', () => {
     expect(item.id).toBe('05023c2908555');
     expect(item.className).toMatch('taskNode d-flex flex-row bd-highlight');
     expect(item.draggable).toBe(true);
+  });
+
+  test('Create task-item in document', () => {
+    const inputTask = {
+      id: '05023c2908555',
+      completed: false,
+      name: 'ThisIsATestingTask',
+      number: 4,
+      current: 0,
+      note: 'ThisIsNotes',
+    };
+
+    const item = new TaskItem.TaskItem(inputTask);
+    document.getElementById('test').appendChild(item);
   });
 });
--- tests/task.test.js
+++ tests/task.test.js
@@ -1,13 +1,53 @@
 const TaskList = require('../source/task');
 
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
 describe('Testing creation of TaskList object with empty allTasks', () => {
-  test('Tasklist test', () => {
+  beforeEach(() => {
     document.body.innerHTML =
+      '<p id="welcome-message"> </p>' +
       '<div id = "test"> <button id="button" />' +
       ' <input type="text" id="task-name">' +
       ' <input type="text" id="task-num">' +
       ' <input type="text" id="task-note">' +
       '</div>';
+    const welcome = document.getElementById('welcome-message');
+    global.localStorage = new LocalStorageMock();
+  });
+
+  test('Creating a task list where allTasks is null', () => {
+    const taskList = new TaskList.TaskList();
+    document.getElementById('test').appendChild(taskList);
+  });
+
+  test('Creating a task list where allTasks is empty', () => {
+    global.localStorage.setItem('allTasks', '{ }');
+    const taskList = new TaskList.TaskList();
+    document.getElementById('test').appendChild(taskList);
+  });
+
+  test('Creating a task list and adding a new task', () => {
+    global.localStorage.setItem('allTasks', '{ }');
     const taskList = new TaskList.TaskList();
     const newButton = document.getElementById('button');
     document.getElementById('task-name').value = 'name';
