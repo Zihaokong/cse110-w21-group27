b52754c06d591d2f5d68eeddfc8bd9f47965e444
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-03-06 17:16:27 -0800

    moved drag and drop and tasklist to be inside TaskList class

--- source/task.js
+++ source/task.js
@@ -3,8 +3,6 @@
  */
 // Section for ESLint
 /* global closeModal */
-let allTasks;
-let dropzone;
 
 /**
  * Class constructor for <task-list>
@@ -17,7 +15,15 @@ class TaskList extends HTMLElement {
       mode: 'open',
     });
 
+    this.allTasks;
+    //variables for drag and drop functions
+    this.dropzone;
+    this.checked = false;
+    this.selectedNode;
+    this.nodes;
+    this.preNodePos;
 
+    //set styles for shadow elements
     shadow.innerHTML = `<link rel="stylesheet" href="task.css"/>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"/>`;
@@ -37,100 +43,44 @@ class TaskList extends HTMLElement {
     this.shadowRoot.append(list);
     const retrievedObject = localStorage.getItem('allTasks');
     if (!retrievedObject || retrievedObject === 'undefined') {
-      allTasks = [];
+      this.allTasks = [];
     } else {
-      allTasks = JSON.parse(retrievedObject);
-      if (allTasks.length !== 0) {
+      this.allTasks = JSON.parse(retrievedObject);
+      if (this.allTasks.length !== 0) {
         document.getElementById('welcome-message').remove();
       }
-      for (let i = 0; i < allTasks.length; i++) {
-        this.renderTask(allTasks[i]);
+      for (let i = 0; i < this.allTasks.length; i++) {
+        this.renderTask(this.allTasks[i]);
       }
     }
-    dropzone = this.shadowRoot.querySelector('ul');
+    
     /// ////// SECTION for Drag and Drop ////////
-  // getter for the list items
-  const nodes = dropzone.getElementsByClassName('taskNode');
-  // variable for the selected node to be dragged or moved
-  let selectedNode;
-  let preNodePos = 0;
-  // variable for the position of selected node
-  let selectedNodePos = 0;
-  let checked = false;
-  dropzone.addEventListener(
+    this.dropzone = this.shadowRoot.querySelector('ul');
+    // getter for the list items
+    this.nodes = this.dropzone.getElementsByClassName('taskNode');
+    // variable for the selected node to be dragged or moved
+    this.selectedNode;
+    this.preNodePos = 0;
+    // variable for the position of selected node
+    //let selectedNodePos = 0;
+    this.checked = false;
+  this.dropzone.addEventListener(
     'dragstart',
-    (event) => {
-      preNodePos = setNodePos(event.clientY, preNodePos);
-      selectedNode = event.target;
-      checked = selectedNode.checkmark.checked;
-    },
+    (event) => this.handleDragStart(event),
     false
   );
 
   // Listener for the dragover event
-  dropzone.addEventListener('dragover', (event) => {
-    event.preventDefault();
-    selectedNodePos = setNodePos(event.clientY, selectedNodePos);
-    if (preNodePos !== selectedNodePos) {
-      preNodePos = setNodePos(event.clientY, preNodePos);
-      dropzone.insertBefore(selectedNode, dropzone.children[selectedNodePos]);
-      selectedNode.checkmark.checked = checked;
-    }
-
-    // re-ordering item in localStorage
-    const newArray = [];
-    for (let i = 0; i < nodes.length; i++) {
-      const targetID = nodes[i].id;
-      const taskInArray = allTasks.find((elem) => elem.id === targetID);
-      newArray.push(taskInArray);
-    }
-    allTasks = newArray;
-  });
+  this.dropzone.addEventListener('dragover', (event) => this.handleDragOver(event));
 
   // Listener for the drop event
-  dropzone.addEventListener('drop', (event) => {
+  this.dropzone.addEventListener('drop', (event) => {
     event.preventDefault();
   });
-
-  /**
-   * For measuring the selected node position from the list.
-   */
-  function establishNodePositions() {
-    for (let i = 0; i < nodes.length; i++) {
-      const position = nodes[i].getBoundingClientRect(); // info of the element position on the frame
-      const yTop = position.top;
-      const yBottom = position.bottom;
-      // yCenter
-      nodes[i].yPos = yTop + (yBottom - yTop) / 2;
-      // nodes[i].yTop = yTop + (yBottom - yTop) / 2;
-      // nodes[i].yBottom = yBottom - (yBottom - yTop) / 2;
-    }
   }
 
-  /**
-   * For deciding which position the selected element goes to as measuring which
-   * is the closest parent and closet children
-   * this function will call establishNodePositions() for selected node position.
-   * @param {event.clickY} currentYPos the y-axis value of the current click on window
-   */
-  function setNodePos(currentYPos, nodePos) {
-    establishNodePositions();
-    let nodeAbove;
-    let currentNodePos = nodePos;
-    for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i].yPos < currentYPos) {
-        nodeAbove = nodes[i];
-        currentNodePos = i + 1;
-      }
-    }
-    // for the top of the list
-    if (typeof nodeAbove === 'undefined') {
-      currentNodePos = 0;
-    }
-
-    return currentNodePos;
-  }
-
+  disconnectedCallback() {
+    localStorage.setItem('allTasks', JSON.stringify(this.allTasks));
   }
 
   setCurrentTask(newTask){
@@ -163,7 +113,7 @@ class TaskList extends HTMLElement {
       current: 0,
       note: document.getElementById('task-note').value,
     };
-    allTasks.push(newTask);
+    this.allTasks.push(newTask);
 
     // render HTML on page.
     this.renderTask(newTask);
@@ -191,8 +141,8 @@ class TaskList extends HTMLElement {
     document.getElementById('task-delete').innerText = `[${name}]`;
     document.getElementById('confirm-button').addEventListener('click', () => {
       for (let i = 0; i < allTasks.length; i++) {
-        if (allTasks[i].name === name) {
-          allTasks.splice(i, 1);
+        if (this.allTasks[i].name === name) {
+          this.allTasks.splice(i, 1);
           break;
         }
       }
@@ -209,7 +159,7 @@ class TaskList extends HTMLElement {
   editTask(event) {
     let editedTask = event.target.getRootNode().host;
     const targetID = editedTask.id;
-    const taskIndex = allTasks.findIndex((elem) => elem.id === targetID);
+    const taskIndex = this.allTasks.findIndex((elem) => elem.id === targetID);
     document.getElementById('edit-note').value = allTasks[taskIndex].note;
     document.getElementById('edit-name').value = editedTask.name;
     document.getElementById('edit-num').value = editedTask.number; 
@@ -224,9 +174,9 @@ class TaskList extends HTMLElement {
       let editTaskNote = document.getElementsByClassName('edit-note').value;
       editedTask.name = editTaskName;
       editedTask.number = editTaskNum; 
-      allTasks[taskIndex].name = editTaskName;
-      allTasks[taskIndex].number = editTaskNum;
-      allTasks[taskIndex].note = editTaskNote;
+      this.allTasks[taskIndex].name = editTaskName;
+      this.allTasks[taskIndex].number = editTaskNum;
+      this.allTasks[taskIndex].note = editTaskNote;
       editModal.style.display = 'none';
       this.removeEventListener('submit', handleEdit);
     });
@@ -251,8 +201,8 @@ class TaskList extends HTMLElement {
       editedTask.completed = 'true';
     }
     // get the element Index in the object list
-    const taskIndex = allTasks.findIndex((elem) => elem.id === targetID);
-    allTasks[taskIndex].completed = !allTasks[taskIndex].completed;
+    const taskIndex = this.allTasks.findIndex((elem) => elem.id === targetID);
+    this.allTasks[taskIndex].completed = !this.allTasks[taskIndex].completed;
   }
 
   /**
@@ -264,24 +214,88 @@ class TaskList extends HTMLElement {
     document.getElementById('play-modal').style.display = 'block';
     const targetTask = event.target.getRootNode().host;
     document.getElementById('timer-name').innerText = targetTask.name;
-    const taskStorageIndex = allTasks.findIndex(
+    const taskStorageIndex = this.allTasks.findIndex(
       (elem) => elem.id === targetTask.id
     );
     // make the note from storage appear in the timer modal
     document.getElementById('timer-note').innerText =
-      allTasks[taskStorageIndex].note;
+      this.allTasks[taskStorageIndex].note;
     // set the current task id to localStorage
     const currentTask = targetTask.id;
     localStorage.setItem('currentTask', JSON.stringify(currentTask));
   }
+
+  handleDragStart(event){
+    this.preNodePos = this.setNodePos(event.clientY, this.preNodePos);
+    this.selectedNode = event.target;
+    this.checked = this.selectedNode.checkmark.checked;
+  }
+
+  handleDragOver(event){
+    event.preventDefault();
+    let selectedNodePos = this.setNodePos(event.clientY, this.selectedNodePos);
+    if (this.preNodePos !== selectedNodePos) {
+      this.preNodePos = this.setNodePos(event.clientY, this.preNodePos);
+      this.dropzone.insertBefore(this.selectedNode, this.dropzone.children[selectedNodePos]);
+      this.selectedNode.checkmark.checked = this.checked;
+    }
+
+    // re-ordering item in localStorage
+    const newArray = [];
+    for (let i = 0; i < this.nodes.length; i++) {
+      const targetID = this.nodes[i].id;
+      const taskInArray = this.allTasks.find((elem) => elem.id === targetID);
+      newArray.push(taskInArray);
+    }
+    this.allTasks = newArray;
+  }
+
+    /**
+   * For measuring the selected node position from the list.
+   */
+  establishNodePositions() {
+    for (let i = 0; i < this.nodes.length; i++) {
+      const position = this.nodes[i].getBoundingClientRect(); // info of the element position on the frame
+      const yTop = position.top;
+      const yBottom = position.bottom;
+      // yCenter
+      this.nodes[i].yPos = yTop + (yBottom - yTop) / 2;
+      // nodes[i].yTop = yTop + (yBottom - yTop) / 2;
+      // nodes[i].yBottom = yBottom - (yBottom - yTop) / 2;
+    }
+  }
+
+    /**
+   * For deciding which position the selected element goes to as measuring which
+   * is the closest parent and closet children
+   * this function will call establishNodePositions() for selected node position.
+   * @param {event.clickY} currentYPos the y-axis value of the current click on window
+   */
+  setNodePos(currentYPos, nodePos) {
+      this.establishNodePositions();
+      let nodeAbove;
+      let currentNodePos = nodePos;
+      for (let i = 0; i < this.nodes.length; i++) {
+        if (this.nodes[i].yPos < currentYPos) {
+          nodeAbove = this.nodes[i];
+          currentNodePos = i + 1;
+        }
+      }
+      // for the top of the list
+      if (typeof nodeAbove === 'undefined') {
+        currentNodePos = 0;
+      }
+  
+      return currentNodePos;
+    }
 }
 customElements.define('task-list', TaskList);
 
 /**
  * Closing page will save current task and update local storage
  */
-window.onbeforeunload = function storeTask() {
-  localStorage.setItem('allTasks', JSON.stringify(allTasks));
+window.onbeforeunload = function removeTaskList() {
+  document.getElementById("main-container").remove();
 };
 
 window.onload = () => {
