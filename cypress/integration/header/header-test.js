f4426535de9d30ad95384bc01451b684f4c582b3
cmwall0605 <cmwall@ucsd.edu>
2021-03-06 22:57:48 -1000

    Added basic cypress tests to header

--- cypress/integration/header/header-test.js
+++ cypress/integration/header/header-test.js
@@ -1,8 +1,14 @@
+const { HeaderComp } = require('../../../source/header-comp');
+
 describe('Header Tests', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5501/source/index.html');
   });
   it('Check for correct header after entering website', () => {
-    cy.get(document.getElementByTagName('header-comp'));
+    cy.get('#header').shadow().find('#date').contains(HeaderComp.createDate());
+    cy.get('#header')
+      .shadow()
+      .find('#completed-cycle')
+      .contains('| Completed Cycles: 0');
   });
 });
