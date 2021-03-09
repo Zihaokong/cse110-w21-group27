eec4763bac4e6d7bd7976637834bb455f035c8a2
cmwall0605 <cmwall@ucsd.edu>
2021-03-09 09:12:24 -1000

    Figured out how to test events by creating mock events

--- __tests__/task.test.js
+++ __tests__/task.test.js
@@ -83,7 +83,7 @@ describe('Test task-list that has pre-existing tasks', () => {
   });
 
   test('Creating a task list with a task inside', () => {
-    const taskList = new TaskList();
+    const taskList = document.createElement('task-list');
     document.getElementById('test').appendChild(taskList);
     expect(
       taskList.shadowRoot
@@ -106,5 +106,52 @@ describe('Test task-list that has pre-existing tasks', () => {
         .getElementById('main-list')
         .querySelectorAll('task-item').length
     ).toBe(3);
+  });
+});
+describe('Test task-list dragging', () => {
+  beforeEach(() => {
+    document.body.innerHTML =
+      '<div id = "welcome-message"> </div>' +
+      '<div id = "add-task-modal"> </div>' +
+      '<div id = "edit-model"> </div>' +
+      '<div id = "delete-modal"> </div>' +
+      '<form id = "taskform"> </form>' +
+      '<div id = "test"> <button id="button" />' +
+      ' <input type="text" id="task-name">' +
+      ' <input type="text" id="task-num">' +
+      ' <input type="text" id="task-note">' +
+      ' <input type="text" id="edit-name">' +
+      ' <input type="text" id="edit-num">' +
+      ' <input type="text" id="edit-note">' +
+      '</div>';
+
+    const allTasks = [];
+    const inputTask1 = {
+      id: '05023c2908555',
+      completed: true,
+      name: 'ThisIsATestingTask',
+      number: 4,
+      current: 0,
+      note: 'ThisIsNotes',
+    };
+    allTasks.push(inputTask1);
+    Storage.prototype.getItem = jest.fn(() => JSON.stringify(allTasks));
+  });
+
+  test('', () => {
+    const taskList = document.createElement('task-list');
+    document.getElementById('test').appendChild(taskList);
+    const taskItem = taskList.shadowRoot.getElementById('05023c2908555');
+    const dropzone = taskList.shadowRoot.querySelector('ul');
+    const event = new Event('dragstart');
+    Object.defineProperty(event, 'target', {
+      writable: false,
+      value: taskItem,
+    });
+    expect(taskList.checked).toBe(false);
+    expect(taskList.selectedNode).toBe(null);
+    dropzone.dispatchEvent(event);
+    expect(taskList.checked).toBe(true);
+    expect(taskList.selectedNode).toBe(taskItem);
   });
 });
