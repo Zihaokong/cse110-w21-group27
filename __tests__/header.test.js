60101b540b2cb398af9a8be69c267a44ca677b31
cmwall0605 <cmwall@ucsd.edu>
2021-03-10 07:24:41 -1000

    added header tests

--- __tests__/header.test.js
+++ __tests__/header.test.js
@@ -1,7 +1,43 @@
 require('../source/header-comp');
 
 describe('Header Testing', () => {
-  test('Create Header', () => {
-    document.createElement('header-comp');
+  test('Create Header where completed = "0"', () => {
+    Storage.prototype.getItem = jest.fn(() => '0');
+    const header = document.createElement('header-comp');
+    document.body.appendChild(header);
+    expect(header.completedCycles).toBe('0');
+    expect(header.cycleCount).toBe(4);
+  });
+
+  test('Create Header where completed = 0', () => {
+    Storage.prototype.getItem = jest.fn(() => 0);
+    const header = document.createElement('header-comp');
+    document.body.appendChild(header);
+    expect(header.completedCycles).toBe(0);
+    expect(header.cycleCount).toBe(4);
+  });
+
+  test('Create Header where completed = 1', () => {
+    Storage.prototype.getItem = jest.fn(() => 1);
+    const header = document.createElement('header-comp');
+    document.body.appendChild(header);
+    expect(header.completedCycles).toBe(1);
+    expect(header.cycleCount).toBe(3);
+  });
+
+  test('Create Header where completed = 4', () => {
+    Storage.prototype.getItem = jest.fn(() => 4);
+    const header = document.createElement('header-comp');
+    document.body.appendChild(header);
+    expect(header.completedCycles).toBe(4);
+    expect(header.cycleCount).toBe(4);
+  });
+
+  test('Create Header where completed = 5', () => {
+    Storage.prototype.getItem = jest.fn(() => 5);
+    const header = document.createElement('header-comp');
+    document.body.appendChild(header);
+    expect(header.completedCycles).toBe(5);
+    expect(header.cycleCount).toBe(3);
   });
 });
