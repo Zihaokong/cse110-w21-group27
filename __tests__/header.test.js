6d8b9a49f43f0a422c149b59e8808d578e24d853
cmwall0605 <cmwall@ucsd.edu>
2021-03-10 23:10:32 -1000

    Added more extensive header testing

--- __tests__/header.test.js
+++ __tests__/header.test.js
@@ -7,14 +7,15 @@ describe('Header Testing', () => {
     document.body.appendChild(header);
     expect(header.completedCycles).toBe('0');
     expect(header.cycleCount).toBe(4);
-  });
-
-  test('Create Header where completed = 0', () => {
-    Storage.prototype.getItem = jest.fn(() => 0);
-    const header = document.createElement('header-comp');
-    document.body.appendChild(header);
-    expect(header.completedCycles).toBe(0);
-    expect(header.cycleCount).toBe(4);
+    expect(
+      header.shadowRoot
+        .getElementById('cycle-count')
+        .querySelectorAll('.filled-dot').length
+    ).toBe(0);
+    expect(
+      header.shadowRoot.getElementById('cycle-count').querySelectorAll('.dot')
+        .length
+    ).toBe(4);
   });
 
   test('Create Header where completed = 1', () => {
@@ -23,6 +24,15 @@ describe('Header Testing', () => {
     document.body.appendChild(header);
     expect(header.completedCycles).toBe(1);
     expect(header.cycleCount).toBe(3);
+    expect(
+      header.shadowRoot
+        .getElementById('cycle-count')
+        .querySelectorAll('.filled-dot').length
+    ).toBe(1);
+    expect(
+      header.shadowRoot.getElementById('cycle-count').querySelectorAll('.dot')
+        .length
+    ).toBe(3);
   });
 
   test('Create Header where completed = 4', () => {
@@ -31,6 +41,15 @@ describe('Header Testing', () => {
     document.body.appendChild(header);
     expect(header.completedCycles).toBe(4);
     expect(header.cycleCount).toBe(4);
+    expect(
+      header.shadowRoot
+        .getElementById('cycle-count')
+        .querySelectorAll('.filled-dot').length
+    ).toBe(4);
+    expect(
+      header.shadowRoot.getElementById('cycle-count').querySelectorAll('.dot')
+        .length
+    ).toBe(0);
   });
 
   test('Create Header where completed = 5', () => {
@@ -39,15 +58,33 @@ describe('Header Testing', () => {
     document.body.appendChild(header);
     expect(header.completedCycles).toBe(5);
     expect(header.cycleCount).toBe(3);
+    expect(
+      header.shadowRoot
+        .getElementById('cycle-count')
+        .querySelectorAll('.filled-dot').length
+    ).toBe(1);
+    expect(
+      header.shadowRoot.getElementById('cycle-count').querySelectorAll('.dot')
+        .length
+    ).toBe(3);
   });
 
-  test('Create Header where completed = 3 - 1000', () => {
+  test('Create Header where completed = 0 - 1000', () => {
     for (let i = 5; i < 1000; i++) {
       Storage.prototype.getItem = jest.fn(() => i);
       const header = document.createElement('header-comp');
       document.body.appendChild(header);
       expect(header.completedCycles).toBe(i);
-      expect(header.cycleCount).toBe(4 - (this.completed % i));
+      expect(header.cycleCount).toBe(4 - (i % 4));
+      expect(
+        header.shadowRoot
+          .getElementById('cycle-count')
+          .querySelectorAll('.filled-dot').length
+      ).toBe(((i - 1) % 4) + 1);
+      expect(
+        header.shadowRoot.getElementById('cycle-count').querySelectorAll('.dot')
+          .length
+      ).toBe(3 - ((i - 1) % 4));
     }
   });
 });
