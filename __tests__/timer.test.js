40b852e6887f7261d812f055ebdd8759d1284d83
Zihaokong <kongzihao99@gmail.com>
2021-03-15 16:33:33 -0700

    tmp

---
+++ __tests__/timer.test.js
@@ -0,0 +1,7 @@
+require('../source/timer_page/timer');
+
+describe('Test Timer functions', () => {
+    test('Creating a task list where allTasks is null', () => {
+      expect(1+2).toBe(3);
+    });
+});
--- source/timer_page/timer.js
+++ source/timer_page/timer.js
@@ -1,114 +1,29 @@
 // number of distraction tracked
-let distractCounter = 0;
+// let distractCounter = 0;
 
-let isFailed = false;
-let isInSession = false;
-let isReload = true; // distinguish refresh or back button
-let currentTaskId;
-let allTasks;
+// let isFailed = false;
+// let isInSession = false;
+// let isReload = true; // distinguish refresh or back button
+// let currentTaskId;
+// let allTasks;
 
-const circle = document.querySelector('.progress-ring-circle');
-const radius = circle.r.baseVal.value;
-const circumference = radius * 2 * Math.PI;
-
-circle.style.strokeDasharray = circumference;
-circle.style.strokeDashoffset = 0;
-
-// progress bar functions
-function setProgress(percent) {
-  const offset = (percent / 100) * circumference;
-  circle.style.strokeDashoffset = -offset;
-}
-
-function resetProgressRing() {
-  circle.style.strokeDashoffset = 0;
-}
-
-// break functions
-function displayBreakComplete() {
-  const audio = new Audio('../../media/break-tune.mp3');
-  audio.play();
-  document.getElementById('breakCompleteModal').style.display = 'block';
-}
-
-function continueTask() {
-  document.getElementById('breakCompleteModal').style.display = 'none';
-  document.body.style.backgroundImage =
-    'linear-gradient(to right,#E0EAFC,#CFDEF3)';
-  document.getElementById('currTask').innerHTML = 'Task';
-  document.getElementById('button-container').style.display = 'flex';
-  document.getElementById('start-btn').style.display = 'block';
-  document.getElementById('button-container').style.paddingLeft = '0px';
-  resetProgressRing();
-  document.getElementById(
-    'distraction-btn'
-  ).innerHTML = `Distraction : ${distractCounter}`;
-  document.getElementById('title_timer').innerHTML = '01:00 - Time To Work!';
-  document.getElementById('minutes').innerHTML = '01';
-  document.getElementById('seconds').innerHTML = '00';
-  document.getElementById('currTask').innerHTML = allTasks[currentTaskId].name;
-  // start(0, 3);
-  // window.location.reload();
-}
-
-function changeTask() {
-  document.getElementById('breakCompleteModal').style.display = 'none';
-  window.location.href = '../index.html';
-}
-
-function displayShortBreak() {
-  const audio1 = new Audio('../../media/work-tune.mp3');
-  audio1.play();
-  setTimeout(() => {
-    resetProgressRing();
-    document.body.style.backgroundImage =
-      'linear-gradient(to right,#74EBD5,#ACB6E5)';
-    document.getElementById('minutes').innerHTML = '01';
-    document.getElementById('seconds').innerHTML = '00';
-    document.getElementById('currTask').innerHTML = 'Short Break';
-    document.getElementById('button-container').style.display = 'none';
-    document.getElementById('container-short').style.display = 'block';
-    // window.location.reload();
-  }, 2000);
-}
-
-function startShortBreak() {
-  document.getElementById('container-short').style.display = 'none';
-  start(0, 3);
-}
-
-function displayLongBreak() {
-  const audio2 = new Audio('../../media/work-tune.mp3');
-  audio2.play();
-  setTimeout(() => {
-    resetProgressRing();
-    document.body.style.backgroundImage =
-      'linear-gradient(to right,#ACB6E5,#74EBD5)';
-    document.getElementById('minutes').innerHTML = '01';
-    document.getElementById('seconds').innerHTML = '00';
-    document.getElementById('currTask').innerHTML = 'Long Break';
-    document.getElementById('button-container').style.display = 'none';
-    document.getElementById('container-long').style.display = 'block';
-    // window.location.reload();
-  }, 2000);
-}
-
-function startLongBreak() {
-  document.getElementById('container-long').style.display = 'none';
-  start(0, 5);
-}
-
-function startTimer() {
-  // enable distraction button during session
-  document.getElementById('distraction-btn').disabled = false;
-  isFailed = true;
-  document.getElementById('start-btn').style.display = 'none';
-  document.getElementById('button-container').style.paddingLeft = '150px';
-  start(0, 3);
-}
+let circle;
+let radius;
+let circumference;
 
 // handle timing
 window.onload = function template() {
+
+  circle = document.querySelector('.progress-ring-circle');
+  radius = circle.r.baseVal.value;
+  circumference = radius * 2 * Math.PI;
+  
+  circle.style.strokeDasharray = circumference;
+  circle.style.strokeDashoffset = 0;
+
+
+
+
   // add event listeners for buttons on timer page
   document.getElementById('start-btn').addEventListener('click', startTimer);
   document
@@ -184,117 +99,214 @@ window.onload = function template() {
   });
 };
 
-/**
- * Set a timer that count down for 60 second.
- * @param {integer} mins minute of timer
- * @param {integer} secs second of timer
- */
 
-function start(mins, secs) {
-  let minutes = mins;
-  let seconds = secs;
-  isInSession = true;
-  const startTime = +new Date();
-  // display correct distraction counter
-  distractCounter = 0;
-  document.getElementById(
-    'distraction-btn'
-  ).innerHTML = `Distraction : ${distractCounter}`;
 
-  const totalsecs = minutes * 60 + seconds;
-  // var progress_ring_interval = setInterval(updateProgressRing, 10);
-  if (minutes < 10) {
-    document.getElementById('minutes').innerHTML = `0${minutes}`;
-  } else {
-    document.getElementById('minutes').innerHTML = minutes;
-  }
-  if (seconds < 10) {
-    document.getElementById('seconds').innerHTML = `0${seconds}`;
-    document.getElementById('title_timer').innerHTML = `${minutes}:0${seconds}`;
-  } else {
-    document.getElementById('seconds').innerHTML = seconds;
-    document.getElementById('title_timer').innerHTML = `${minutes}:${seconds}`;
-  }
 
-  // var minutes_interval = setInterval(minutesTimer, 60000);
-  const secondsInterval = setInterval(secondsTimer, 500);
+// progress bar functions
+// function setProgress(percent) {
+//   const offset = (percent / 100) * circumference;
+//   circle.style.strokeDashoffset = -offset;
+// }
 
-  function secondsTimer() {
-    document.getElementById('header').isNewCycle = 'false';
-    const currTime = +new Date();
-    const elapsed = Math.floor((currTime - startTime) / 1000);
-    const timeLeft = totalsecs - elapsed;
-    minutes = Math.floor(timeLeft / 60);
-    seconds = timeLeft % 60;
+// function resetProgressRing() {
+//   circle.style.strokeDashoffset = 0;
+// }
 
-    const perc = 100 - (timeLeft / totalsecs) * 100;
-    setProgress(perc);
-    if (minutes < 10) {
-      document.getElementById('minutes').innerHTML = `0${minutes}`;
-    } else {
-      document.getElementById('minutes').innerHTML = minutes;
-    }
-    if (seconds < 10) {
-      document.getElementById('seconds').innerHTML = `0${seconds}`;
-      document.getElementById(
-        'title_timer'
-      ).innerHTML = `${minutes}:0${seconds}`;
-      if (seconds === 0) {
-        if (minutes <= 0) {
-          // clearInterval(minutes_interval);
-          clearInterval(secondsInterval);
+// // break functions
+// function displayBreakComplete() {
+//   const audio = new Audio('../../media/break-tune.mp3');
+//   audio.play();
+//   document.getElementById('breakCompleteModal').style.display = 'block';
+// }
 
-          let counter = Number(localStorage.getItem('sessionCounter'));
-          counter += 1;
-          const pomo = localStorage.getItem('isPomo');
-          isInSession = false;
+// function continueTask() {
+//   document.getElementById('breakCompleteModal').style.display = 'none';
+//   document.body.style.backgroundImage =
+//     'linear-gradient(to right,#E0EAFC,#CFDEF3)';
+//   document.getElementById('currTask').innerHTML = 'Task';
+//   document.getElementById('button-container').style.display = 'flex';
+//   document.getElementById('start-btn').style.display = 'block';
+//   document.getElementById('button-container').style.paddingLeft = '0px';
+//   resetProgressRing();
+//   document.getElementById(
+//     'distraction-btn'
+//   ).innerHTML = `Distraction : ${distractCounter}`;
+//   document.getElementById('title_timer').innerHTML = '01:00 - Time To Work!';
+//   document.getElementById('minutes').innerHTML = '01';
+//   document.getElementById('seconds').innerHTML = '00';
+//   document.getElementById('currTask').innerHTML = allTasks[currentTaskId].name;
+//   // start(0, 3);
+//   // window.location.reload();
+// }
 
-          // disable distraction button
-          document.getElementById('distraction-btn').disabled = true;
-          if (pomo === 'true') {
-            localStorage.setItem('isPomo', 'false');
-            // clear all circles for work session following longbreak
-            if (localStorage.getItem('LongBreak') === 'true') {
-              document.getElementById('header').isNewCycle = 'true';
-            }
+// function changeTask() {
+//   document.getElementById('breakCompleteModal').style.display = 'none';
+//   window.location.href = '../index.html';
+// }
 
-            localStorage.setItem('ShortBreak', 'false');
-            localStorage.setItem('LongBreak', 'false');
-            displayBreakComplete();
-          } else {
-            localStorage.setItem('isPomo', 'true');
+// function displayShortBreak() {
+//   const audio1 = new Audio('../../media/work-tune.mp3');
+//   audio1.play();
+//   setTimeout(() => {
+//     resetProgressRing();
+//     document.body.style.backgroundImage =
+//       'linear-gradient(to right,#74EBD5,#ACB6E5)';
+//     document.getElementById('minutes').innerHTML = '01';
+//     document.getElementById('seconds').innerHTML = '00';
+//     document.getElementById('currTask').innerHTML = 'Short Break';
+//     document.getElementById('button-container').style.display = 'none';
+//     document.getElementById('container-short').style.display = 'block';
+//     // window.location.reload();
+//   }, 2000);
+// }
 
-            // hide the fail modal if the timer runs out
-            document.getElementById('failModal').style.display = 'none';
-            isFailed = false;
-            if (counter % 4 === 0) {
-              document.getElementById('header').completedCycles = counter;
-              localStorage.setItem('sessionCounter', counter);
-              localStorage.setItem('LongBreak', 'true');
-              localStorage.setItem('ShortBreak', 'false');
-              displayLongBreak();
-            } else {
-              document.getElementById('header').completedCycles = counter;
-              localStorage.setItem('sessionCounter', counter);
-              localStorage.setItem('ShortBreak', 'true');
-              localStorage.setItem('LongBreak', 'false');
-              displayShortBreak();
-            }
+// function startShortBreak() {
+//   document.getElementById('container-short').style.display = 'none';
+//   start(0, 3);
+// }
 
-            // update progress for current task
-            allTasks[currentTaskId].current += 1;
-            localStorage.setItem('allTasks', JSON.stringify(allTasks));
-          }
-        }
-      }
-    } else {
-      document.getElementById('seconds').innerHTML = seconds;
-      document.getElementById(
-        'title_timer'
-      ).innerHTML = `${minutes}:${seconds}`;
-    }
-  }
-}
+// function displayLongBreak() {
+//   const audio2 = new Audio('../../media/work-tune.mp3');
+//   audio2.play();
+//   setTimeout(() => {
+//     resetProgressRing();
+//     document.body.style.backgroundImage =
+//       'linear-gradient(to right,#ACB6E5,#74EBD5)';
+//     document.getElementById('minutes').innerHTML = '01';
+//     document.getElementById('seconds').innerHTML = '00';
+//     document.getElementById('currTask').innerHTML = 'Long Break';
+//     document.getElementById('button-container').style.display = 'none';
+//     document.getElementById('container-long').style.display = 'block';
+//     // window.location.reload();
+//   }, 2000);
+// }
+
+// function startLongBreak() {
+//   document.getElementById('container-long').style.display = 'none';
+//   start(0, 5);
+// }
+
+// function startTimer() {
+//   // enable distraction button during session
+//   document.getElementById('distraction-btn').disabled = false;
+//   isFailed = true;
+//   document.getElementById('start-btn').style.display = 'none';
+//   document.getElementById('button-container').style.paddingLeft = '150px';
+//   start(0, 3);
+// }
+
+
+// /**
+//  * Set a timer that count down for 60 second.
+//  * @param {integer} mins minute of timer
+//  * @param {integer} secs second of timer
+//  */
+
+// function start(mins, secs) {
+//   let minutes = mins;
+//   let seconds = secs;
+//   isInSession = true;
+//   const startTime = +new Date();
+//   // display correct distraction counter
+//   distractCounter = 0;
+//   document.getElementById(
+//     'distraction-btn'
+//   ).innerHTML = `Distraction : ${distractCounter}`;
+
+//   const totalsecs = minutes * 60 + seconds;
+//   // var progress_ring_interval = setInterval(updateProgressRing, 10);
+//   if (minutes < 10) {
+//     document.getElementById('minutes').innerHTML = `0${minutes}`;
+//   } else {
+//     document.getElementById('minutes').innerHTML = minutes;
+//   }
+//   if (seconds < 10) {
+//     document.getElementById('seconds').innerHTML = `0${seconds}`;
+//     document.getElementById('title_timer').innerHTML = `${minutes}:0${seconds}`;
+//   } else {
+//     document.getElementById('seconds').innerHTML = seconds;
+//     document.getElementById('title_timer').innerHTML = `${minutes}:${seconds}`;
+//   }
+
+//   // var minutes_interval = setInterval(minutesTimer, 60000);
+//   const secondsInterval = setInterval(secondsTimer, 500);
+
+//   function secondsTimer() {
+//     document.getElementById('header').isNewCycle = 'false';
+//     const currTime = +new Date();
+//     const elapsed = Math.floor((currTime - startTime) / 1000);
+//     const timeLeft = totalsecs - elapsed;
+//     minutes = Math.floor(timeLeft / 60);
+//     seconds = timeLeft % 60;
+
+//     const perc = 100 - (timeLeft / totalsecs) * 100;
+//     setProgress(perc);
+//     if (minutes < 10) {
+//       document.getElementById('minutes').innerHTML = `0${minutes}`;
+//     } else {
+//       document.getElementById('minutes').innerHTML = minutes;
+//     }
+//     if (seconds < 10) {
+//       document.getElementById('seconds').innerHTML = `0${seconds}`;
+//       document.getElementById(
+//         'title_timer'
+//       ).innerHTML = `${minutes}:0${seconds}`;
+//       if (seconds === 0) {
+//         if (minutes <= 0) {
+//           // clearInterval(minutes_interval);
+//           clearInterval(secondsInterval);
+
+//           let counter = Number(localStorage.getItem('sessionCounter'));
+//           counter += 1;
+//           const pomo = localStorage.getItem('isPomo');
+//           isInSession = false;
+
+//           // disable distraction button
+//           document.getElementById('distraction-btn').disabled = true;
+//           if (pomo === 'true') {
+//             localStorage.setItem('isPomo', 'false');
+//             // clear all circles for work session following longbreak
+//             if (localStorage.getItem('LongBreak') === 'true') {
+//               document.getElementById('header').isNewCycle = 'true';
+//             }
+
+//             localStorage.setItem('ShortBreak', 'false');
+//             localStorage.setItem('LongBreak', 'false');
+//             displayBreakComplete();
+//           } else {
+//             localStorage.setItem('isPomo', 'true');
+
+//             // hide the fail modal if the timer runs out
+//             document.getElementById('failModal').style.display = 'none';
+//             isFailed = false;
+//             if (counter % 4 === 0) {
+//               document.getElementById('header').completedCycles = counter;
+//               localStorage.setItem('sessionCounter', counter);
+//               localStorage.setItem('LongBreak', 'true');
+//               localStorage.setItem('ShortBreak', 'false');
+//               displayLongBreak();
+//             } else {
+//               document.getElementById('header').completedCycles = counter;
+//               localStorage.setItem('sessionCounter', counter);
+//               localStorage.setItem('ShortBreak', 'true');
+//               localStorage.setItem('LongBreak', 'false');
+//               displayShortBreak();
+//             }
+
+//             // update progress for current task
+//             allTasks[currentTaskId].current += 1;
+//             localStorage.setItem('allTasks', JSON.stringify(allTasks));
+//           }
+//         }
+//       }
+//     } else {
+//       document.getElementById('seconds').innerHTML = seconds;
+//       document.getElementById(
+//         'title_timer'
+//       ).innerHTML = `${minutes}:${seconds}`;
+//     }
+//   }
+// }
 
 function distractionCount() {
   distractCounter += 1;
@@ -327,3 +339,8 @@ window.onbeforeunload = function WarnReload() {
     return 'Your timer progress will reset';
   }
 };
+
+if (typeof exports !== 'undefined') {
+  module.exports = quitFailModal;
+
+}
