ad353d7788cbf418d08d7a680e847328ceb05f1a
Chad Wall <cmwall@ucsd.edu>
2022-05-20 16:30:46 -0700

    Removed unused js from the main file now that most of the modals were removed (info no longer works, but that's going to b reworked shortly

--- source/main.js
+++ source/main.js
@@ -2,35 +2,40 @@
  * This file defines functions and implements the behaviors for pop-up modals
  * and other Modals for the main page.
  */
+
 // Array of stat info objects; these objects include the day, pomo count,
 // # of distractions, and completed pomos.
 let statsList;
 
-// Variables for elements which are found in window load.
-let modal;
-let btns;
-let cancelBtns;
-let spanClose;
+document.addEventListener('DOMContentLoaded', () => {
+  // set the timer state back to a work session
+  localStorage.setItem('ShortBreak', 'false');
+  localStorage.setItem('LongBreak', 'false');
 
-// Set the onload function to be handleLoad.
-window.onload = handleLoad;
+  // Previous session info if it exists
+  const retrievedStats = localStorage.getItem('statsList');
+  if (!retrievedStats || retrievedStats === 'undefined') {
+    statsList = [];
+  } else {
+    statsList = JSON.parse(retrievedStats);
+    determineSessionDate();
+  }
+});
 
-/**
- * Function which sets the modal's display to be block
- */
-function openModal() {
-  modal.style.display = 'block';
-}
+window.addEventListener('beforeunload', () => {
+  // call in case the date has changed before the next pomo
+  determineSessionDate();
 
-/**
- * Function which sets the openInfoModal's display to be block
- */
-function openInfoModal() {
-  document.getElementById('info-modal').style.display = 'block';
-}
-function closeInfoModal() {
-  document.getElementById('info-modal').style.display = 'none';
-}
+  // create current Date object
+  const current = new Date();
+  localStorage.setItem(
+    'lastVisit',
+    JSON.stringify(current.toLocaleDateString('en-US'))
+  );
+  if (document.getElementsByTagName('task-list')[0]) {
+    document.getElementsByTagName('task-list')[0].remove();
+  }
+});
 
 /**
  * To determine the current date when the user accessing to the index.html.
@@ -75,121 +80,4 @@ function determineSessionDate() {
     // adding the list with new stat to localStorage
     localStorage.setItem('statsList', JSON.stringify(statsList));
   }
-}
-
-/*
- * For scroll to the top, used in Top button
- */
-function scrollFunc() {
-  window.scrollTo(0, 0);
-}
-
-/**
- * Close the Modal with X button or cancel button
- */
-function closeModal() {
-  modal.style.display = 'none';
-  document.getElementById('play-modal').style.display = 'none';
-  document.getElementById('edit-modal').style.display = 'none';
-  document.getElementById('delete-modal').style.display = 'none';
-}
-
-/**
- * Close the modal via event
- * @param {event} event: Javascript events
- */
-function eventCloseModal(event) {
-  switch (event.target) {
-    case modal:
-      modal.style.display = 'none';
-      break;
-    case document.getElementById('delete-modal'):
-      document.getElementById('delete-modal').style.display = 'none';
-      break;
-    case document.getElementById('edit-modal'):
-      document.getElementById('edit-modal').style.display = 'none';
-      break;
-    case document.getElementById('info-modal'):
-      document.getElementById('info-modal').style.display = 'none';
-      break;
-    case document.getElementById('play-modal'):
-      document.getElementById('play-modal').style.display = 'none';
-      break;
-    default:
-  }
-}
-
-/**
- * Called on window load, sets the stats list from local storage, finds elements
- * in the browser DOM to attach to the element variables, and attaches event
- * listeners to them which, based on their usage, will open or close the modal.
- */
-function handleLoad() {
-  // set the timer state back to a work session
-  localStorage.setItem('ShortBreak', 'false');
-  localStorage.setItem('LongBreak', 'false');
-
-  // Get the add-task-modal
-  modal = document.getElementById('add-task-modal');
-  // Get the button that opens the modal
-  btns = document.getElementsByClassName('add-task-btn');
-  // Get the cancel button that close the modal
-  cancelBtns = document.getElementsByClassName('cancel-btn');
-  // Get the <span> element that closes the modal
-  spanClose = document.getElementsByClassName('close');
-  const retrievedStats = localStorage.getItem('statsList');
-  if (!retrievedStats || retrievedStats === 'undefined') {
-    statsList = [];
-  } else {
-    statsList = JSON.parse(retrievedStats);
-    determineSessionDate();
-  }
-
-  // add event listeners for closing Modal
-  for (let i = 0; i < spanClose.length; ++i) {
-    spanClose[i].addEventListener('click', closeModal);
-    cancelBtns[i].addEventListener('click', closeModal);
-  }
-  // Listener for displaying add task modal
-  for (let i = 0; i < btns.length; ++i) {
-    btns[i].addEventListener('click', openModal);
-  }
-
-  // close the modal when clicking outside
-  window.onclick = eventCloseModal;
-
-  window.onbeforeunload = handleUnload;
-}
-
-/**
- * Closing or leaving page will remove the TaskList object,
- * trigger determineSessionDate(), and log the currentDate time
- */
-function handleUnload() {
-  // call in case the date has changed before the next pomo
-  determineSessionDate();
-
-  // create current Date object
-  const current = new Date();
-  localStorage.setItem(
-    'lastVisit',
-    JSON.stringify(current.toLocaleDateString('en-US'))
-  );
-  if (document.getElementsByTagName('task-list')[0]) {
-    document.getElementsByTagName('task-list')[0].remove();
-  }
-}
-
-if (typeof exports !== 'undefined') {
-  module.exports = {
-    openModal,
-    determineSessionDate,
-    closeModal,
-    eventCloseModal,
-    openInfoModal,
-    closeInfoModal,
-    handleLoad,
-    handleUnload,
-    scrollFunc,
-  };
 }
