385ad82b23fa736bba087badf0b0bfaad60fef34
Kaiwen Tsou <kktsou@ucsd.edu>
2022-05-24 15:20:03 -0700

    Added placeholder modal using new style

---
+++ source/timer-page/tutorial.js
@@ -0,0 +1,16 @@
+// If we only need to pop up this single modal, then
+// putting this in header-comp might be smarter.
+if (localStorage.getItem('isNoob') === 'true') {
+  document.getElementById('tutorialModal').style.display = 'block';
+} else {
+  // do nothing
+}
+
+document.getElementById('tutorialModal').onclick = () => {
+  // clicked inside modal
+  document.getElementById('tutorialModal').style.display = 'none';
+};
+document.querySelector('#tutorialModal > div').onclick = function (event) {
+  // clicked outside modal
+  event.stopPropagation();
+};
