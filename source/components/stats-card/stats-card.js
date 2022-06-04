04d19723e984893a57bf3f5785e80a288d986ac9
Chad Wall <cmwall@ucsd.edu>
2022-06-03 19:08:32 -0700

    Styling fix

--- source/components/stats-card/stats-card.js
+++ source/components/stats-card/stats-card.js
@@ -1,6 +1,6 @@
 /**
  * \<stats-card\>
- * 
+ *
  * A webcomponent used to display statistic information on the stats page.
  *
  * The StatsCard represents a single statistic, and contains an image, a number,
@@ -13,11 +13,11 @@ class StatsCard extends HTMLElement {
    */
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
+    this.attachShadow({ mode: 'open' });
 
-    const styleSheet = document.createElement("link");
-    styleSheet.rel = "stylesheet";
-    styleSheet.href = "/components/stats-card/stats-card.css";
+    const styleSheet = document.createElement('link');
+    styleSheet.rel = 'stylesheet';
+    styleSheet.href = '/components/stats-card/stats-card.css';
 
     this.shadowRoot.appendChild(styleSheet);
   }
@@ -26,16 +26,10 @@ class StatsCard extends HTMLElement {
    * Called when the webcomponent is applied to the DOM; Sets up the webcomponent.
    */
   connectedCallback() {
-    const {
-      srcUrl,
-      altText,
-      imgWidth,
-      imgHeight,
-      stat,
-      cardTitle,
-    } = this.getContent();
+    const { srcUrl, altText, imgWidth, imgHeight, stat, cardTitle } =
+      this.getContent();
 
-    const template = document.createElement("template");
+    const template = document.createElement('template');
     template.innerHTML = `
             <article class="block-contents">
                 <img src="${srcUrl}" alt="${altText}" width="${imgWidth}" height="${imgHeight}" />
@@ -63,37 +57,37 @@ class StatsCard extends HTMLElement {
    * @returns {StatContent}
    */
   getContent() {
-    const statType = this.getAttribute("stat-type");
-    let srcUrl = "";
-    let altText = "";
-    let imgWidth = "";
-    let imgHeight = "";
-    let cardTitle = "";
-    let stat = "";
+    const statType = this.getAttribute('stat-type');
+    let srcUrl = '';
+    let altText = '';
+    let imgWidth = '';
+    let imgHeight = '';
+    let cardTitle = '';
+    let stat = '';
 
     switch (statType) {
-      case "completed":
-        srcUrl = "/assets/images/check.png";
-        altText = "checkmark symbol indicating completed pomos";
-        imgWidth = "40";
-        imgHeight = "39";
-        cardTitle = "completed pomos";
+      case 'completed':
+        srcUrl = '/assets/images/check.png';
+        altText = 'checkmark symbol indicating completed pomos';
+        imgWidth = '40';
+        imgHeight = '39';
+        cardTitle = 'completed pomos';
         stat = this.calculateStat().completedPomos;
         break;
-      case "distractions":
-        srcUrl = "/assets/images/warning.png";
-        altText = "warning symbol indicating average distractions per pomo";
-        imgWidth = "39";
-        imgHeight = "38";
-        cardTitle = "average distractions per pomo";
+      case 'distractions':
+        srcUrl = '/assets/images/warning.png';
+        altText = 'warning symbol indicating average distractions per pomo';
+        imgWidth = '39';
+        imgHeight = '38';
+        cardTitle = 'average distractions per pomo';
         stat = this.calculateStat().avgDistractions;
         break;
-      case "success":
-        srcUrl = "/assets/images/star.png";
-        altText = "star symbol indicating pomo success rate";
-        imgWidth = "39";
-        imgHeight = "39";
-        cardTitle = "pomo success rate";
+      case 'success':
+        srcUrl = '/assets/images/star.png';
+        altText = 'star symbol indicating pomo success rate';
+        imgWidth = '39';
+        imgHeight = '39';
+        cardTitle = 'pomo success rate';
         stat = this.calculateStat().successRate;
         break;
       default:
@@ -113,18 +107,18 @@ class StatsCard extends HTMLElement {
   /**
    * Goes through the statsList in localStorage and calculates all the
    * relevant data for the stats being tracked in the given time period.
-   * 
+   *
    * The time period is established via the stat-length attribute.
-   * 
+   *
    * @returns {StatValues}
    */
   calculateStat() {
-    const statLength = this.getAttribute("stat-length");
-    const statsList = JSON.parse(localStorage.getItem("statsList")) ?? [];
+    const statLength = this.getAttribute('stat-length');
+    const statsList = JSON.parse(localStorage.getItem('statsList')) ?? [];
     const today = new Date();
-    let numPomos = Number(localStorage.getItem("todayPomo"));
-    let completedPomos = Number(localStorage.getItem("sessionCounter"));
-    let distractions = Number(localStorage.getItem("distractCounter"));
+    let numPomos = Number(localStorage.getItem('todayPomo'));
+    let completedPomos = Number(localStorage.getItem('sessionCounter'));
+    let distractions = Number(localStorage.getItem('distractCounter'));
 
     // eslint-disable-next-line no-restricted-syntax
     for (const statsItem of statsList) {
@@ -143,11 +137,11 @@ class StatsCard extends HTMLElement {
       }
     }
 
-    let avgDistractions = "";
-    let successRate = "";
+    let avgDistractions = '';
+    let successRate = '';
     if (completedPomos === 0) {
-      avgDistractions = "0";
-      successRate = "0%";
+      avgDistractions = '0';
+      successRate = '0%';
     } else {
       avgDistractions = (distractions / numPomos).toFixed(1);
       successRate = `${((completedPomos / numPomos) * 100).toFixed(2)}%`;
@@ -158,15 +152,15 @@ class StatsCard extends HTMLElement {
 
   /**
    * Recalculates the stat value for this card.
-   * 
+   *
    * Called by the stats-page after the Reset stats button is pressed.
    */
   reset() {
     const { stat } = this.getContent();
 
-    const value = this.shadowRoot.getElementById("stat");
+    const value = this.shadowRoot.getElementById('stat');
     value.textContent = stat;
   }
 }
 
-window.customElements.define("stats-card", StatsCard);
+window.customElements.define('stats-card', StatsCard);
